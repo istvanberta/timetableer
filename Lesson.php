b0b74ac1fb64331a21d91a14e6e3384cb0185521
istvanberta <istvan.berta@gmail.com>
2018-12-01 19:37:26 +0100

    Work has begun on LessonCollection.

--- Lesson.php
+++ Lesson.php
@@ -2,20 +2,15 @@
 
 class Lesson
 {
-    private $class;
-    private $day;
-    private $period;
-    private $subject;
-    private $teacher;
+    public $id;
+    public $day;
+    public $period;
+    public $schoolClass;
+    public $subject;
+    public $teacher;
 
     public function __construct()
     {
-        // TODOCREATE TABLE IF NOT EXISTS `mydb`.`teachers` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(45) NOT NULL,
-  `last_name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
+        
     }
 }
---
+++ LessonCollection.php
@@ -0,0 +1,29 @@
+<?php
+
+class LessonCollection implements Iterator
+{
+    public function current()
+    {
+        
+    }
+
+    public function key()
+    {
+
+    }
+
+    public function next()
+    {
+        
+    }
+
+    public function rewind()
+    {
+        
+    }
+
+    public function valid()
+    {
+        
+    }
+}
