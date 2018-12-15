80abe31c272c0882fea36afd695f1ae8436806fd
istvanberta <istvan.berta@gmail.com>
2018-12-15 21:03:31 +0100

    LessonCollection implemented.

--- LessonCollection.php
+++ LessonCollection.php
@@ -2,28 +2,42 @@
 
 class LessonCollection implements Iterator
 {
+    private $position;
+    private $lessons;
+
+    public function __construct()
+    {
+        $this->position = 0;
+        $this->lessons = [];
+    }
+
+    public function add(Lesson $lesson)
+    {
+        $this->lessons[] = $lesson;
+    }
+
     public function current()
     {
-        
+        return $this->lessons[$this->position];
     }
 
     public function key()
     {
-
+        return $this->position;
     }
 
     public function next()
     {
-        
+        $this->position++;
     }
 
     public function rewind()
     {
-        
+        $this->position = 0;
     }
 
     public function valid()
     {
-        
+        return isset($this->lessons[$this->position]);
     }
 }
