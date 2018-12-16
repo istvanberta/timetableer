60d4851798cf351924d387fd634e3ec0eab54610
istvanberta <istvan.berta@gmail.com>
2018-12-16 21:53:19 +0100

    findById function extracted/abstracted into Mapper.

--- Mapper.php
+++ Mapper.php
@@ -9,4 +9,20 @@ abstract class Mapper
         $reg = Registry::instance();
         $this->pdo = $reg->getPdo();
     }
+
+    public function findById(int $id): ?Entity
+    {
+        $stmt = $this->findByIdStmt();
+        $stmt->execute([$id]);
+        $row = $stmt->fetch();
+
+        if (!$row) {
+            return null;
+        }
+
+        return $this->mapRowToEntity($row);
+    }
+
+    abstract protected function findByIdStmt(): PDOStatement;
+    abstract protected function mapRowToEntity(array $row): Entity;
 }
