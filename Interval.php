e106c22525aa121396a16d1f43f179d49ca3034d
istvanberta <istvan.berta@gmail.com>
2018-12-28 19:56:23 +0100

    Interval now uses Linux timestamp internally.

--- Interval.php
+++ Interval.php
@@ -5,9 +5,19 @@ class Interval
     private $startTime;
     private $endTime;
 
-    public function __construct(int $startTime, int $endTime)
+    public function __construct(string $startTime, string $endTime)
     {
-        $this->startTime = $startTime;
-        $this->endTime = $endTime;
+        $this->startTime = strtotime($startTime);
+        $this->endTime = strtotime($endTime);
+    }
+
+    public function getStartTime(): int
+    {
+        return $this->startTime;
+    }
+
+    public function getEndTime(): int
+    {
+        return $this->endTime;
     }
 }
