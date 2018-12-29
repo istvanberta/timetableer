279dd69efb90d4c78f548c288674acf1d2c7cfdb
istvanberta <istvan.berta@gmail.com>
2018-12-29 21:16:51 +0100

    ClasseMapper rewrite.

--- ClasseMapper.php
+++ ClasseMapper.php
@@ -2,12 +2,40 @@
 
 class ClasseMapper extends Mapper
 {
+    private $findByIdStmt;
+    private $insertStmt;
+    private $updateStmt;
+    private $deleteStmt;
+
+    public function findById(int $id): ?Classe
+    {
+        $this->findByIdStmt()->execute([$id]);
+        $row = $this->findByIdStmt()->fetch();
+
+        if (!$row) {
+            return null;
+        }
+
+        return $this->mapRowToClasse($row);
+    }
+
+    private function findByIdStmt(): PDOStatement
+    {
+        if (!isset($this->findByIdStmt)) {
+            $this->findByIdStmt = $this->pdo->prepare($this->findByIdStmt());
+        }
+
+        return $this->findByIdStmt;
+    }
+
+    private function sqlForFindById(): string
+    {
+        return 'SELECT id, abbrev, name FROM classes WHERE id = ?';
+    }
+
     public function insert(Classe $classe)
     {
-        $sql = 'INSERT INTO classes (abbrev, name) VALUES (?, ?)';
-
-        $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([
+        $this->insertStmt()->execute([
             $classe->getAbbrev(),
             $classe->getName()
         ]);
@@ -15,35 +43,67 @@ class ClasseMapper extends Mapper
         $this->setEntityId($classe, $this->pdo->lastInsertId());
     }
 
+    private function insertStmt(): PDOStatement
+    {
+        if (!isset($this->insertStmt)) {
+            $this->insertStmt = $this->pdo->prepare($this->sqlForInsert());
+        }
+
+        return $this->insertStmt;
+    }
+
+    private function sqlForInsert(): string
+    {
+        return 'INSERT INTO classes (abbrev, name) VALUES (?, ?)';
+    }
+
     public function update(Classe $classe)
     {
-        $sql = 'UPDATE classes SET abbrev = ?, name = ? WHERE id = ?';
-
-        $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([
+        $this->updateStmt()->execute([
             $classe->getAbbrev(),
             $classe->getName(),
             $classe->getId()
         ]);
     }
 
+    private function updateStmt(): PDOStatement
+    {
+        if (!isset($this->updateStmt)) {
+            $this->updateStmt = $this->pdo->prepare($this->sqlForUpdate());
+        }
+
+        return $this->updateStmt;
+    }
+
+    private function sqlForUpdate(): string
+    {
+        return 'UPDATE classes SET abbrev = ?, name = ? WHERE id = ?';
+    }
+
     public function delete(int $id)
     {
-        $sql = 'DELETE FROM classes WHERE id = ?';
-
-        $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([$id]);
+        $this->deleteStmt()->execute([$id]);
     }
 
-    protected function findByIdStmt(): PDOStatement
+    private function deleteStmt(): PDOStatement
     {
-        $sql = 'SELECT id, abbrev, name FROM classes WHERE id = ?';
+        if (!isset($this->deleteStmt)) {
+            $this->deleteStmt = $this->pdo->prepare($this->sqlForDelete());
+        }
 
-        return $this->pdo->prepare($sql);
+        return $this->deleteStmt;
     }
 
-    protected function mapRowToEntity(array $row): Entity
+    private function sqlForDelete(): string
     {
-        return new Classe($row['abbrev'], $row['name']);
+        return 'DELETE FROM classes WHERE id = ?';
+    }
+
+    private function mapRowToClasse(array $row): Classe
+    {
+        $classe = new Classe($row['abbrev'], $row['name']);
+        $this->setEntityId($classe, $row['id']);
+
+        return $classe;
     }
 }
