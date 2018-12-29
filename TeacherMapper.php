f5e382c3984187e613d752622d8aa317f2efb52e
istvanberta <istvan.berta@gmail.com>
2018-12-29 17:25:02 +0100

    TeacherMapper rewrite.

--- TeacherMapper.php
+++ TeacherMapper.php
@@ -2,49 +2,116 @@
 
 class TeacherMapper extends Mapper
 {
+    private $findByIdStmt;
+    private $insertStmt;
+    private $updateStmt;
+    private $deleteStmt;
+
     public function findById(int $id): ?Teacher
     {
-        $sql = 'SELECT id, first_name, last_name FROM teachers WHERE id = ?';
+        $this->findByIdStmt()->execute([$id]);
+        $row = $this->findByIdStmt()->fetch();
 
-        $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([$id]);
-        $result = $stmt->fetch();
-
-        if (!$result) {
+        if (!$row) {
             return null;
         }
 
-        return $this->mapRowToTeacher($result);
+        return $this->mapRowToTeacher($row);
+    }
+
+    private function findByIdStmt(): PDOStatement
+    {
+        if (!isset($this->findByIdStmt)) {
+            $this->findByIdStmt = $this->pdo->prepare($this->sqlForFindById());
+        }
+
+        return $this->findByIdStmt;
+    }
+
+    private function sqlForFindById(): string
+    {
+        return 'SELECT id, first_name, last_name, acronym 
+                FROM teachers WHERE id = ?'; 
     }
 
     public function insert(Teacher $teacher)
     {
-        $sql = 'INSERT INTO teachers (first_name, last_name) VALUES (?, ?)';
+        $this->insertStmt()->execute([
+            $teacher->getName()->getFirstName(),
+            $teacher->getName()->getLastName(),
+            $teacher->getAcronym()
+        ]);
 
-        $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([$teacher->firstName, $teacher->lastName]);
+        $this->setEntityId($teacher, $this->pdo->lastInsertId());
+    }
 
-        $teacher->id = $this->pdo->lastInsertId();
+    private function insertStmt(): PDOStatement
+    {
+        if (!isset($this->insertStmt)) {
+            $this->insertStmt = $this->pdo->prepare($this->sqlForInsert());
+        }
+
+        return $this->insertStmt;
+    }
+
+    private function sqlForInsert(): string
+    {
+        return 'INSERT INTO teachers (first_name, last_name, acronym) 
+                VALUES (?, ?, ?)';
     }
 
     public function update(Teacher $teacher)
     {
-        $sql = 'UPDATE teachers SET first_name = ?, last_name = ? WHERE id = ?';
+        $this->updateStmt()->execute([
+            $teacher->getName()->getFirstName(),
+            $teacher->getName()->getLastName(),
+            $teacher->getAcronym(),
+            $teacher->getId()
+        ]);
+    }
 
-        $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([$teacher->firstName, $teacher->lastName, $teacher->id]);
+    private function updateStmt(): PDOStatement
+    {
+        if (!isset($this->updateStmt)) {
+            $this->updateStmt = $this->pdo->prepare($this->sqlForUpdate());
+        }
+
+        return $this->updateStmt;
+    }
+
+    private function sqlForUpdate(): string
+    {
+        return 'UPDATE teachers 
+                SET first_name = ?, last_name = ?, acronym = ? 
+                WHERE id = ?';
     }
 
     public function delete(int $id)
     {
-        $sql = 'DELETE FROM teachers WHERE id = ?';
-
-        $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([$id]);
+        $this->deleteStmt()->execute([$id]);
     }
 
-    protected function mapRowToTeacher(array $row): Teacher
+    private function deleteStmt(): PDOStatement
     {
-        return new Teacher($row['first_name'], $row['last_name'], $row['id']);
+        if (!isset($this->deleteStmt)) {
+            $this->deleteStmt = $this->pdo->prepare($this->sqlForDelete());
+        }
+
+        return $this->deleteStmt;
+    }
+
+    private function sqlForDelete(): string
+    {
+        return 'DELETE FROM teachers WHERE id = ?';
+    }
+
+    private function mapRowToTeacher(array $row): Teacher
+    {
+        $name = new Name($row['first_name'], $row['last_name']);
+        $teacher = new Teacher($name);
+        $teacher->setAcronym($row['acronym']);
+        $this->setEntityId($teacher, $row['id']);
+
+        return $teacher;
     }
 }
