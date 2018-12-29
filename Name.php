8fbd488820b738d1e47788e53008c4aa9e7865d0
istvanberta <istvan.berta@gmail.com>
2018-12-29 12:46:10 +0100

    Name object created.

---
+++ Name.php
@@ -0,0 +1,28 @@
+<?php
+
+class Name
+{
+    private $firstName;
+    private $lastName;
+
+    public function __construct(string $firstName, string $lastName)
+    {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function getName(): string
+    {
+        return $this->firstName . ' ' . $this->lastName;
+    }
+}
