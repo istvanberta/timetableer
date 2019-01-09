50a8ff558b04579d349efdf233095dd71ad03a9e
istvanberta <istvan.berta@gmail.com>
2019-01-10 00:15:43 +0100

    Autoloader made namespace compatible.

--- autoload.php
+++ autoload.php
@@ -1,9 +1,11 @@
 <?php
 
 $basicAutoloader = function ($classname) {
+    $pos = strpos($classname, '\\'); // find namespace prefix end
+    $classname = substr($classname, $pos + 1); //strip namespace prefix
     $file = __DIR__ . "/" . "{$classname}.php";
     if (file_exists($file)) {
-        require_once($file);
+        require($file);
     }
 };
 
