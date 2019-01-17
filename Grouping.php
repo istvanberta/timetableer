d8ebf5e836f755a28173de5186811dbb4567a63e
istvanberta <istvan.berta@gmail.com>
2019-01-17 23:00:41 +0100

    Work has begun on Iterator implementation for Grouping class.

--- Grouping.php
+++ Grouping.php
@@ -17,4 +17,29 @@ class Grouping implements \Iterator
     {
         $this->groups[] = $group;
     }
+
+    public function current(): Group
+    {
+        return $this->groups[$this->position];
+    }
+
+    public function key(): int
+    {
+        return $this->position;
+    }
+
+    public function next()
+    {
+        $this->position++;
+    }
+
+    public function rewind()
+    {
+        $this->position = 0;
+    }
+
+    public function valid(): bool
+    {
+        return isset($this->groups[$this->position]);
+    }
 }
